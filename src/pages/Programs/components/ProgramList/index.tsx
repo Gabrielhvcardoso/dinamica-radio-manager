6a99dfda36866dfa67817d3814a142694f1e165d
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-11-27 14:18:50 -0300

    no programs results

--- src/pages/Programs/components/ProgramList/index.tsx
+++ src/pages/Programs/components/ProgramList/index.tsx
@@ -10,6 +10,11 @@ const ProgramList: React.FC = () => {
   return (
     <List>
       {
+        !programs[0]
+          ? <p style={{ marginLeft: 10, color: '#bbb', cursor: 'default' }}>Não há resultados</p>
+          : <></>
+      }
+      {
         programs.map((program) => (
           <Program key={program.programId} program={program} />
         ))
